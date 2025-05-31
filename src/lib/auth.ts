cfa654633a27bc63080f8dcabb3a7f28ba5d57b7
ibrahimijai <sunday.m1701072@st.futminna.edu.ng>
2025-05-31 17:12:19 +0100

    addec prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,9 @@
 // Read more here: https://better-auth.vercel.app/docs/integrations/next
 
-
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./prisma";
 
 export const auth = betterAuth({
   // https://better-auth.vercel.app/docs/authentication/twitter
@@ -13,4 +14,7 @@ export const auth = betterAuth({
     },
   },
   plugins: [nextCookies()], // make sure this is the last plugin in the array
+  database: prismaAdapter(prisma, {
+    provider: "sqlite",
+  }),
 });
